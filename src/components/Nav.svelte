340779a802f399eabcd2a2670a5d36d75a9b3201
connorgaskell <48499463+connorgaskell@users.noreply.github.com>
2020-11-03 17:26:10 +0000

    Updated README and added added Nav component.

---
+++ src/components/Nav.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let address;
+
+    let addressText = () => {
+        if(address === undefined) {
+            return "Create Account";
+        } else {
+            return address;
+        }
+    }
+</script>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div>
+        {addressText()}
+    </div>
+</nav>
